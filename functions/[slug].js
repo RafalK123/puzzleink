84246798ddf51b307401d5be75efa41915d4275e
Rafal K. <rafal.kolakowski@gmail.com>
2026-03-13 11:50:27 +0100

    Update [slug.js] Worker [slug].js niech nie ustawia query param, tylko po prostu zwraca slug.html i niech JS w slug.html odczytuje slug z pathname.

--- functions/[slug].js
+++ functions/[slug].js
@@ -1,17 +1,15 @@
 export async function onRequest(context) {
   const slug = context.params.slug;
 
-  // Jeśli slug jest pusty lub "free-coloring", zwracamy galerię
   if (!slug || slug.toLowerCase() === "free-coloring") {
     const url = new URL(context.request.url);
     url.pathname = "/free-coloring/index.html";
     return context.env.ASSETS.fetch(url.toString());
   }
 
-  // W przeciwnym razie przekierowujemy do slug.html
+  // fetch slug.html, ale bez ?file=slug
   const url = new URL(context.request.url);
   url.pathname = "/free-coloring/slug.html";
-  url.searchParams.set("file", slug);
 
   const newRequest = new Request(url.toString(), context.request);
 
