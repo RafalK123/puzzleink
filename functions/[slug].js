f0cbed71b677ff35aac3171e76f527c782c9ace0
Rafal K. <rafal.kolakowski@gmail.com>
2026-03-13 11:09:45 +0100

    Fix slug worker to match only /free-coloring/<slug>

--- functions/[slug].js
+++ functions/[slug].js
@@ -1,6 +1,17 @@
 export async function onRequest(context) {
+  const path = new URL(context.request.url).pathname;
 
-  const slug = context.params.slug;
+  // Sprawdzenie czy ścieżka pasuje do /free-coloring/<slug>
+  const slugMatch = path.match(/^\/free-coloring\/([^\/]+)\/?$/);
+
+  if (!slugMatch) {
+    // jeśli to nie slug, zwróć galerię
+    const url = new URL(context.request.url);
+    url.pathname = "/free-coloring/index.html";
+    return context.env.ASSETS.fetch(url.toString());
+  }
+
+  const slug = slugMatch[1];
 
   const url = new URL(context.request.url);
   url.pathname = "/free-coloring/slug.html";
@@ -9,5 +20,4 @@ export async function onRequest(context) {
   const newRequest = new Request(url.toString(), context.request);
 
   return context.env.ASSETS.fetch(newRequest);
-
 }
